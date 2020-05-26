0bda5feff8ad71bd6bf3502636db056e4084d180
RopFoo <ropfoo@gmail.com>
2020-05-26 12:58:44 +0200

    added adress search

--- ez-gmaps.js
+++ ez-gmaps.js
@@ -5,7 +5,7 @@ const ezMap = {
   apiURL: 'https://maps.googleapis.com/maps/api/geocode/json',
   apiKey: '',
   address: '',
-  response: '',
+  response: undefined,
   createMap(zoom, centerCoords, id, imgURL = undefined) {
     this.imgURL = imgURL;
 
@@ -49,17 +49,6 @@ const ezMap = {
       });
     }
   },
-  addMarkerFromAddress(address, apiKey) {
-    this.address = address;
-    this.apiKey = apiKey;
-    APIRequest(formatURL(this.apiURL, this.address, this.apiKey))
-      .then((posts) => {
-        const responseData = JSON.parse(posts.response);
-        this.response = responseData.results[0].geometry.location;
-        console.log(this.response);
-      })
-      .catch((error) => console.log('oh no!', error));
-  },
   generateMarkers(markers) {
     markers.forEach((marker) => {
       this.addMarker(
@@ -78,35 +67,58 @@ const ezMap = {
     });
     this.generateMarkers(this.markers);
   },
+  markerBoilerPlate(markerImport, coords) {
+    return {
+      icon: markerImport.dataset.icon ? markerImport.dataset.icon : this.imgURL,
+      iconActive: markerImport.dataset.iconactive,
+      isActive: markerImport.dataset.isactive === 'true' ? true : false,
+      title: markerImport.dataset.title,
+      content: markerImport.dataset.content,
+      coords: coords,
+    };
+  },
   loadInMarkersFromMarkup(id) {
     const markersImport = document.getElementById(id).children;
     Array.from(markersImport).forEach((markerImport) => {
-      this.markers.push({
-        icon: markerImport.dataset.icon
-          ? markerImport.dataset.icon
-          : this.imgURL,
-        iconActive: markerImport.dataset.iconactive,
-        isActive: markerImport.dataset.isactive === 'true' ? true : false,
-        title: markerImport.dataset.title,
-        content: markerImport.dataset.content,
-        coords: {
-          lat: parseFloat(markerImport.dataset.lat),
-          lng: parseFloat(markerImport.dataset.lng),
-        },
-      });
+      if (markerImport.dataset.address) {
+        this.address = { location: markerImport.dataset.address };
+        APIRequest(formatURL(this.apiURL, this.address, this.apiKey))
+          .then((posts) => {
+            const responseData = JSON.parse(posts.response);
+            this.response = responseData.results[0].geometry.location;
+            this.markers.push(
+              this.markerBoilerPlate(markerImport, {
+                lat: this.response.lat,
+                lng: this.response.lng,
+              })
+            );
+          })
+          .then(() => {
+            this.generateMarkers(this.markers);
+          })
+          .catch((error) => console.log('oh no!', error));
+      } else {
+        this.markers.push(
+          this.markerBoilerPlate(markerImport, {
+            lat: parseFloat(markerImport.dataset.lat),
+            lng: parseFloat(markerImport.dataset.lng),
+          })
+        );
+      }
     });
     this.generateMarkers(this.markers);
   },
   createMarkerContent(title, content) {
     return `<b>${title}</b></br><p>${content}</p>`;
   },
+  setAPIKey(apiKey) {
+    this.apiKey = apiKey;
+  },
 };
-
 const formatURL = (url, address, apiKey) => {
-  return `${url}?address=${address.cityName}&key=${apiKey}`;
+  return `${url}?address=${address.location}&key=${apiKey}`;
 };
-
-const APIRequest = (url, method) => {
+function APIRequest(url, method) {
   const request = new XMLHttpRequest();
   return new Promise((resolve, reject) => {
     request.onreadystatechange = () => {
@@ -120,4 +132,4 @@ const APIRequest = (url, method) => {
     request.open(method || 'GET', url, true);
     request.send();
   });
-};
+}
